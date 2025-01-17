eabe4574616395b5dd8c6e189d117ec6998f7064
ioan.clarke <ioan.clarke@autotrader.co.uk>
2025-01-17 19:30:52 +0000

    add api tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use axum::routing::patch;
+use axum::routing::{delete, patch};
 use axum::{
     routing::{get, post},
     Router,
 };
+use axum::response::{IntoResponse, Response};
+use http::StatusCode;
 use rust_crud::db::init_db;
 use rust_crud::users::{create_user, get_all_users, get_user, patch_user};
 
@@ -14,8 +16,14 @@ async fn main() {
         .route("/users", post(create_user))
         .route("/users", get(get_all_users))
         .route("/users/{user_id}", get(get_user))
-        .route("/users/{user_id}", patch(patch_user));
+        .route("/users/{user_id}", patch(patch_user))
+        .route("/db", delete(reset_db));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn reset_db() -> Response {
+    init_db();
+    StatusCode::NO_CONTENT.into_response()
 }
