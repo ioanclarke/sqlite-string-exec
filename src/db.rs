1cbe79f443bffbf78fe85ff8912da63637b96027
ioan.clarke <ioan.clarke@autotrader.co.uk>
2025-01-17 18:47:23 +0000

    implement string methods for db queries

--- src/db.rs
+++ src/db.rs
@@ -1,13 +1,46 @@
-use crate::users::{UserCreate, User, UserPatch};
+use crate::users::{User, UserCreate, UserPatch};
 use lazy_static::lazy_static;
-use rusqlite::{params, Connection, Error, OptionalExtension, Row};
-use std::sync::Mutex;
-use uuid::Uuid;
+use rusqlite::{params, Connection, Error, OptionalExtension, Params, Row, ToSql};
+use std::sync::{Mutex, MutexGuard};
+
+pub fn init_db() {
+    DROP_USER_TABLE.execute();
+    CREATE_USER_TABLE.execute();
+    INSERT_USER.execute_with_params(params![1, "John"]);
+}
+
+pub fn insert_user(user: UserCreate) -> User {
+    let user = User::new(user.username);
+    INSERT_USER.execute_with_params(params![user.id, user.username]);
+    user
+}
+
+pub fn get_all_users() -> Vec<User> {
+    SELECT_ALL_USERS.query_many()
+}
+
+pub fn get_user(id: String) -> Option<User> {
+    SELECT_USER_BY_ID.query_one(params![id])
+}
+
+pub fn update_user(id: String, patch: UserPatch) -> Option<User> {
+    UPDATE_USER_BY_ID.execute_with_params(params![id, patch.username]);
+    SELECT_USER_BY_ID.query_one(params![id])
+}
 
 lazy_static! {
     pub static ref DB_CONN: Mutex<Connection> = Mutex::new(Connection::open("users.db").unwrap());
 }
 
+fn get_conn() -> MutexGuard<'static, Connection> {
+    DB_CONN.lock().unwrap()
+}
+
+const DROP_USER_TABLE: &str = "DROP TABLE IF EXISTS users";
+const CREATE_USER_TABLE: &str = "CREATE TABLE IF NOT EXISTS users (
+                                    id TEXT PRIMARY KEY,
+                                    username TEXT NOT NULL
+                                )";
 const INSERT_USER: &str = "INSERT INTO users (id, username) VALUES (?1, ?2)";
 const SELECT_ALL_USERS: &str = "SELECT id, username FROM users";
 const SELECT_USER_BY_ID: &str = "SELECT id, username FROM users where id = ?1";
@@ -20,63 +53,53 @@ static MAP_ROW_TO_USER: fn(&Row) -> Result<User, Error> = |row: &Row| {
     })
 };
 
-pub fn init_db() {
-    let conn = DB_CONN.lock().unwrap();
-    conn.execute("DROP TABLE IF EXISTS users", []).unwrap();
-
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS users (
-            id TEXT PRIMARY KEY,
-            username TEXT NOT NULL
-        )",
-        [],
-    )
-    .unwrap();
-
-    conn.execute(INSERT_USER, params![1, "John"]).unwrap();
+trait Executable<T> {
+    fn execute(&self);
+    fn execute_with_params<P>(&self, params: P)
+    where
+        P: IntoIterator + Params,
+        P::Item: ToSql;
+    fn query_many(&self) -> Vec<T>;
+    fn query_one<P>(&self, params: P) -> Option<T>
+    where
+        P: IntoIterator + Params,
+        P::Item: ToSql;
 }
 
-pub fn insert_user(user: UserCreate) -> User {
-    let conn = DB_CONN.lock().unwrap();
-    
-    let user = User {
-        id: String::from(Uuid::new_v4()),
-        username: user.username,
-    };
-    conn.execute(INSERT_USER, params![user.id, user.username])
-        .unwrap();
+impl Executable<User> for &str {
+    fn execute(&self) {
+        let conn = get_conn();
+        conn.execute(&self, []).unwrap();
+    }
 
-    user
-}
+    fn execute_with_params<P>(&self, params: P)
+    where
+        P: IntoIterator + Params,
+        P::Item: ToSql,
+    {
+        let conn = get_conn();
+        conn.execute(&self, params).unwrap();
+    }
 
-pub fn get_all_users() -> Vec<User> {
-    let conn = DB_CONN.lock().unwrap();
-    let mut stmt = conn.prepare(SELECT_ALL_USERS).unwrap();
+    fn query_many(&self) -> Vec<User> {
+        let conn = get_conn();
+        let mut stmt = conn.prepare(SELECT_ALL_USERS).unwrap();
 
-    stmt.query_map(params![], MAP_ROW_TO_USER)
-        .unwrap()
-        .collect::<rusqlite::Result<Vec<User>>>()
-        .unwrap()
-}
+        stmt.query_map(params![], MAP_ROW_TO_USER)
+            .unwrap()
+            .collect::<rusqlite::Result<Vec<User>>>()
+            .unwrap()
+    }
+    fn query_one<P>(&self, params: P) -> Option<User> 
+    where
+        P: IntoIterator + Params,
+        P::Item: ToSql,
+    {
+        let conn = DB_CONN.lock().unwrap();
+        let mut stmt = conn.prepare(SELECT_USER_BY_ID).unwrap();
 
-pub fn get_user(id: String) -> Option<User> {
-    let conn = DB_CONN.lock().unwrap();
-    let mut stmt = conn.prepare(SELECT_USER_BY_ID).unwrap();
-
-    stmt.query_row(params![String::from(id)], MAP_ROW_TO_USER)
-        .optional()
-        .unwrap()
-}
-
-pub fn update_user(id: String, patch: UserPatch) -> User {
-    let conn = DB_CONN.lock().unwrap();
-    
-    conn.execute(UPDATE_USER_BY_ID, params![id, patch.username]).unwrap();
-
-    let mut stmt = conn.prepare(SELECT_USER_BY_ID).unwrap();
-
-    stmt.query_row(params![String::from(id)], MAP_ROW_TO_USER)
-        .optional()
-        .unwrap()
-        .unwrap()
+        stmt.query_row(params, MAP_ROW_TO_USER)
+            .optional()
+            .unwrap()
+    }
 }
--- src/users.rs
+++ src/users.rs
@@ -5,11 +5,21 @@ use axum::Json;
 use http::StatusCode;
 use rust_patch::Patch;
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Serialize)]
 pub struct User {
     pub(crate) id: String,
     pub(crate) username: String,
+}
+
+impl User {
+    pub(crate) fn new(username: String) -> User {
+        User {
+            id: String::from(Uuid::new_v4()),
+            username,
+        }
+    }
 }
 
 #[derive(Deserialize, Eq, PartialEq)]
@@ -34,15 +44,18 @@ pub async fn get_all_users() -> (StatusCode, Json<Vec<User>>) {
 }
 
 pub async fn get_user(Path(user_id): Path<String>) -> Response {
-    let user = db::get_user(user_id);
-
-    match user {
-        Some(user) => Json(user).into_response(),
-        None => StatusCode::NOT_FOUND.into_response(),
-    }
+    let maybe_user = db::get_user(user_id);
+    get_or_404(maybe_user)
 }
 
-pub async fn patch_user(Path(user_id): Path<String>, Json(patch): Json<UserPatch>) -> Json<User> {
-    let user = db::update_user(user_id, patch);
-    Json(user)
+pub async fn patch_user(Path(user_id): Path<String>, Json(patch): Json<UserPatch>) -> Response {
+    let maybe_user = db::update_user(user_id, patch);
+    get_or_404(maybe_user)
+}
+
+fn get_or_404<T: Serialize>(maybe_entity: Option<T>) -> Response {
+    match maybe_entity {
+        Some(entty) => Json(entty).into_response(),
+        None => StatusCode::NOT_FOUND.into_response(),
+    }
 }
